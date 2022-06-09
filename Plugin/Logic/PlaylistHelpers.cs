fceb4f13db35350f84870f8ce2664f044724bb82
DSikes <david@sikespc.com>
2022-06-08 20:52:32 -0400

    Finished organizing playlistHelpers and TrackHelpers

---
+++ Plugin/Logic/PlaylistHelpers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace MusicBeePlugin.Logic
+{
+    public class PlaylistHelpers
+    {
+        private readonly Plugin.MusicBeeApiInterface _mbApi;
+
+        public PlaylistHelpers(Plugin.MusicBeeApiInterface mbApi)
+        {
+            _mbApi = mbApi;
+        }
+        
+        public List<string> GetAllNonRadioPlaylistUris()
+        {
+            var allPlaylistUris = new List<string>();
+
+            _mbApi.Playlist_QueryPlaylists();
+
+            var currentPlaylistUri = _mbApi.Playlist_QueryGetNextPlaylist(); 
+
+            while (currentPlaylistUri != null)
+            {
+                Debug.WriteLine($"Now loading playlist: {Path.GetFileNameWithoutExtension(currentPlaylistUri)}");
+                
+                var currentPlaylistType = _mbApi.Playlist_GetType(currentPlaylistUri);
+
+                if (currentPlaylistType != Plugin.PlaylistFormat.Radio)
+                {
+                    allPlaylistUris.Add(currentPlaylistUri);
+                }
+                
+                currentPlaylistUri = _mbApi.Playlist_QueryGetNextPlaylist();
+            }
+
+            return allPlaylistUris;
+        }
+        
+        public List<string> GetAllFilePathsIn(string playlistUri)
+        {
+            var allPlaylistTracks = new List<string>();
+
+            _mbApi.Playlist_QueryFiles(playlistUri);
+
+            var currentPlaylistTrack = _mbApi.Playlist_QueryGetNextFile(); 
+
+            while (currentPlaylistTrack != null)
+            {
+                allPlaylistTracks.Add(currentPlaylistTrack);
+                
+                currentPlaylistTrack = _mbApi.Playlist_QueryGetNextFile();
+            }
+
+            return allPlaylistTracks;
+        }
+    }
+}
---
+++ Plugin/Logic/TrackFetcher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MusicBeePlugin.Models;
+using TagLib;
+
+namespace MusicBeePlugin.Logic
+{
+    public class TrackFetcher
+    {
+        public Track LoadTrackData(string trackFilePath)
+        {
+            var returnTrack = new Track();
+
+            var trackTagFile = TagLib.File.Create(trackFilePath);
+
+            // Album
+            returnTrack.Album = trackTagFile.Tag.Album;
+            
+            // Artist
+            returnTrack.Artist = trackTagFile.Tag.Performers[0];
+            
+            // Album Artist
+            returnTrack.AlbumArtist = ReadAlbumArtistFromTag(trackTagFile);
+
+            // Title
+            returnTrack.Title = trackTagFile.Tag.Title;
+            
+            // Rating
+            returnTrack.Rating = ReadRatingFromTag(trackTagFile);
+            
+            // File Path
+            returnTrack.FilePath = trackFilePath;
+
+            return returnTrack;
+        }
+
+        private string ReadAlbumArtistFromTag(File trackTagFile)
+        {
+            var artists = trackTagFile.Tag.AlbumArtists;
+
+            if (artists.Length == 0) return "";
+            
+            return trackTagFile.Tag.AlbumArtists[0];
+        }
+
+        private static int ReadRatingFromTag(File trackTagFile)
+        {
+            var custom = (TagLib.Ogg.XiphComment)trackTagFile.GetTag(TagLib.TagTypes.Xiph);
+            
+            var ratingAsStrings = custom.GetField("RATING");
+
+            if (ratingAsStrings.Length == 0)
+            {
+                return 0;
+            }
+
+            if (ratingAsStrings[0].Contains("."))
+            {
+                var parsedDouble = double.Parse(ratingAsStrings[0]);
+
+                var convertedInt = (int)(parsedDouble * 10);
+                
+                return convertedInt;
+            }
+            
+            return int.Parse(ratingAsStrings[0]);
+        }
+    }
+}
