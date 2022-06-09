fad600eb2d93b34bbe96b98f3f7ea96489292707
DSikes <david@sikespc.com>
2022-06-08 20:52:02 -0400

    Added models for playlist and track

---
+++ Plugin/Models/Playlist.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MusicBeePlugin.Models
+{
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public string FileUri { get; set; }
+        
+        public List<Track> Tracks { get; set; }
+    }
+}
---
+++ Plugin/Models/Track.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicBeePlugin.Models
+{
+    public class Track
+    {
+        public string FilePath { get; set; }
+        
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string AlbumArtist { get; set; }
+        public string Album { get; set; }
+        public int Rating { get; set; }
+    }
+}
